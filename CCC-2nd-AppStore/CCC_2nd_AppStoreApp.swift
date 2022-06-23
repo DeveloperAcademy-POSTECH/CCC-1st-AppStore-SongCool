ed7b718d4f496cf2ad141e5c9b9defa7fc476979
song-cool <oscar2978@naver.com>
2022-06-24 00:23:37 +0900

    FEAT: init, gitignore, gitattributes 추가

---
+++ CCC-2nd-AppStore/CCC_2nd_AppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCC_2nd_AppStoreApp.swift
+//  CCC-2nd-AppStore
+//
+//  Created by 송시원 on 2022/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CCC_2nd_AppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
